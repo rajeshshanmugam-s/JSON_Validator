6a8018e1fa9c87e28cb5509d470feb966cca53a2
rajeshshanmugam <rajeshshanmugam@positivenaick.com>
2020-08-21 16:19:47 +0530

    Add App layout

--- routes.py
+++ routes.py
@@ -7,7 +7,13 @@ app = flk.Flask(__name__)
 def test():
     return "Working"
 
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return flk.render_template('index.html', results=flk.request.form['text'])
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
 
