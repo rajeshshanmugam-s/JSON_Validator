b8253f8356cd513a44eb7c3b1d8c481119add133
rajeshshanmugam <rajeshshanmugam@positivenaick.com>
2020-08-21 19:15:57 +0530

    Add validator

--- routes.py
+++ routes.py
@@ -1,4 +1,6 @@
 import flask as flk
+import json
+import requests
 
 app = flk.Flask(__name__)
 
@@ -10,7 +12,13 @@ def test():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return flk.render_template('index.html', results=flk.request.form['text'])
+    try:
+        raw_json = flk.request.form.get('text', "default text")
+        valid_json = json.loads(raw_json)
+    except ValueError:
+        result = "Not a valid Json"
+        return flk.render_template('index.html', results=result)
+    return flk.render_template('index.html', results=valid_json)
 
 
 if __name__ == "__main__":
